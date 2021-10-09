9b41cab9b51e104130994690d16b5727906f8e11
Lachimolalah <92054359+Lachimolalah@users.noreply.github.com>
2021-10-09 18:32:06 +0200

    Update WaterPollution.vb

--- GroupProject/GroupProject/WaterPollution.vb
+++ GroupProject/GroupProject/WaterPollution.vb
@@ -8,10 +8,12 @@
 ' Class name: (WaterPollution)
 ' *****************************************************************
 
+'Option statements
 Option Strict On
 Option Explicit On
 Option Infer Off
 
+'Derived class
 Public Class WaterPollution
 
     Inherits Factories_Industries
@@ -19,6 +21,7 @@ Public Class WaterPollution
     Private _Bio As BioDiversity
     Private _SizeOfArea, _Concentration As Double
 
+    'Property methods
     Public Property Bio As BioDiversity
         Set(value As BioDiversity)
             _Bio = value
@@ -46,18 +49,32 @@ Public Class WaterPollution
         End Get
     End Property
 
+    'Constructor
     Public Sub New(nFactors As Integer, Name As String, Description As String, Profit As Double, sizeofarea As Double, concentration As Double)
         MyBase.New(nFactors, Name, Description, Profit)
         _SizeOfArea = sizeofarea
         _Concentration = concentration
     End Sub
 
-    'Functions
+    'Methods
+    'Function to calculate Pollution level based on the concentration of pollution *  by size of polluted area
     Public Function PollutionLevel() As Double
         Return (_SizeOfArea * _Concentration)
     End Function
 
+    'Overrided function to Calculate Charges based on Pollution level * size of polluted area
     Public Overrides Function Charges() As Double
         Return (_SizeOfArea * PollutionLevel())
     End Function
+    
+    'Function to display details of the Waterpollution class
+    Public Overrides Function Display() As String
+        Dim disp As String = " "
+        disp &= "Waterpollution: " &  MyBase.Display & Environment.Newline
+        disp &= "Pollution Level: " & PollutionLevel() & Environment.Newline
+        disp &= "Charges: " & Charges() & Environment.Newline
+        return disp
+    End function
 End Class
+
+
